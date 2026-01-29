f38859ba4dfe3dc1212ce4f42c7c506b5d61e4c2
lj-lin1 <3059748226@qq.com>
2026-01-29 09:54:36 +0800

    合并wyh-2026/1/29-9:54

--- Drivers/BSP/Protocol/protocol.h
+++ Drivers/BSP/Protocol/protocol.h
@@ -14,17 +14,6 @@
 #define FRAME_CMD_OFFSET  (2U)
 #define FRAME_LEN_OFFSET  (3U)
 
-typedef struct frame_struct {
-    uint32_t head;
-    uint32_t seq;
-    uint32_t cmd;
-    uint32_t len;
-    uint32_t data_crc[];
-} IAP_Frame_t;
-
-void handle_protocol(void);
-bool check_frame_validity(const RingBuffer *buff, uint32_t *data_len, uint8_t *cmd_num);
-
 __attribute__((packed)) typedef struct frame_struct {
     uint32_t head;       // 帧头
     uint32_t seq;        // 帧序号
@@ -32,5 +21,8 @@ __attribute__((packed)) typedef struct frame_struct {
     uint32_t len;        // 数据长度（data长度）(单位:word,1word=4byte)
     uint32_t data_crc[]; // 柔性数组，数据区
 } IAP_Frame_t;
+
+void handle_protocol(void);
+bool check_frame_validity(const RingBuffer *buff, uint32_t *data_len, uint8_t *cmd_num);
 
 #endif // !DRIVERS_BSP_PROTOCOL_PROTOCOL_H
